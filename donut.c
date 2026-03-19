562d1d813c5e6ddaf5ed74bc3e9fce7376f5126f
Luiz Felipe Grochevski <luizfelipegrochevski@gmail.com>
2026-03-19 13:56:28 -0300

    Corrigindo código

--- donut.c
+++ donut.c
@@ -1,32 +1,52 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
-int k;
+int main(void) {
+    int k;
+    float A = 0, B = 0, i, j, z[1760];
+    char b[1760];
 
-double sin(), cos();
+    printf("\x1b[2J");
 
-int main() {
-	float A = 0, B = 0, i, j, z[1760];
-	char b[1760];
-	printf("\n1b[2J");
-	for(;;) {
-		memset(b, 32, 1760);
-		memset(z, 0, 7040);
-		for(j = 0; 6.28 > j; j += 0.01) {
-			for(i = 0; 6.28 > i; i += 0.08) {
-				float c = sin(i), d = cos(j), e = sin(A), f = sin(j), g = cos(A), h = d + 2, D = 1 / (c * h * e + f * g + 5), l = cos(i), m = cos(B), n = sin(B), t = c * h * g - f * c;
-				int x = 40 + 30 * D * (l * h * m - t * n);
-				int y = 12 + 15 * D * (l * h * n + t * m), o = x + 80 * y, N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
-				if (22 > y && y > 0 && 80 > x && D > z[o]) {
-					z[o] = D;
-					b[o] = ":;-~:;=!^#50:"[N > 0 ? N : 0];
-				}
-			}
-			printf("\x1b[H");
-			for (k = 0; 1761 > k; k++)
-				putchar(k % 80 ? b[k] : 10);
-			A += 0.04;
-			B += 0.02;
-		}
-	}
+    for (;;) {
+        memset(b, 32, sizeof(b));
+        memset(z, 0, sizeof(z));
+
+        for (j = 0; j < 6.28; j += 0.07) {
+            for (i = 0; i < 6.28; i += 0.02) {
+                float c = sinf(i);
+                float d = cosf(j);
+                float e = sinf(A);
+                float f = sinf(j);
+                float g = cosf(A);
+                float h = d + 2.0f;
+                float D = 1.0f / (c * h * e + f * g + 5.0f);
+                float l = cosf(i);
+                float m = cosf(B);
+                float n = sinf(B);
+                float t = c * h * g - f * e;
+
+                int x = 40 + (int)(30 * D * (l * h * m - t * n));
+                int y = 12 + (int)(15 * D * (l * h * n + t * m));
+                int o = x + 80 * y;
+                int N = (int)(8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n));
+
+                if (x > 0 && x < 80 && y > 0 && y < 22 && D > z[o]) {
+                    z[o] = D;
+                    b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
+                }
+            }
+        }
+
+        printf("\x1b[H");
+        for (k = 0; k < 1760; k++) {
+            putchar(k % 80 ? b[k] : '\n');
+        }
+
+        A += 0.04f;
+        B += 0.02f;
+    }
+
+    return 0;
 }
